1f8d96d9f628adbe937076e46682c8b8b98ecbfa
Mike Ingber <michael.ingber@wework.com>
2019-10-21 19:48:20 -0400

    Updates

---
+++ precache-manifest.f7d411b591c3023621660305cafbdc81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a65f5dba6ff9c3b9022361cc37c20cf",
+    "url": "/torus/index.html"
+  },
+  {
+    "revision": "bf3c3004583c406ece2f",
+    "url": "/torus/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2c7bd4eb31673da29044",
+    "url": "/torus/static/js/2.fced043c.chunk.js"
+  },
+  {
+    "revision": "bf3c3004583c406ece2f",
+    "url": "/torus/static/js/main.e80fbbc1.chunk.js"
+  },
+  {
+    "revision": "ffdb630bbecec7f39105",
+    "url": "/torus/static/js/runtime-main.1f0e9ca9.js"
+  }
+]);
